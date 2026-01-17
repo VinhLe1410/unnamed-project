c80a82c7e1f56feb4ef2027da390d5454699d7d2
Phuc Vinh Le <lpvinh2k4@gmail.com>
2026-01-17 22:37:22 +1100

    chore: add redirect for non-authenticated access

---
+++ src/routes/(protected)/dashboard/+page.svelte
@@ -0,0 +1,9 @@
+<div class="min-h-screen bg-background p-8">
+  <div class="mx-auto max-w-4xl">
+    <h1 class="text-center text-4xl font-bold text-foreground">Dashboard Page</h1>
+    <h2 class="text-2xt mt-4 text-center font-semibold text-foreground">Authentication Level: Protected</h2>
+    <p class="mt-4 text-center text-muted-foreground">
+      If you can see this without being logged in, something's cooked
+    </p>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <div class="min-h-screen bg-background p-8">
   <div class="mx-auto max-w-4xl">
-    <h1 class="text-center text-4xl font-bold text-foreground">Welcome</h1>
+    <h1 class="text-center text-4xl font-bold text-foreground">Welcome Page</h1>
+    <h2 class="text-2xt mt-4 text-center font-semibold text-foreground">Authentication Level: Open</h2>
     <p class="mt-4 text-center text-muted-foreground">First Ever Project using SvelteKit</p>
   </div>
 </div>
